9e0aa32b194592e4925744d40409ae7a30ba4de6
Olivia <oliviaflexx@gmail.com>
2021-08-16 10:42:01 -0400

    updated static root

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, '_staticfiles')
 
 STATIC_URL = '/static/'
 
@@ -144,3 +145,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 INTERNAL_IPS = [
     '127.0.0.1'
 ]
+
