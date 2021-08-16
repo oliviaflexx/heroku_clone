c25b37cbf46069b5bf96b9cf44cf29878af8107b
Olivia <oliviaflexx@gmail.com>
2021-08-16 12:49:25 -0400

    pandas csv 5

--- main/views.py
+++ main/views.py
@@ -346,63 +346,63 @@ def addData(response):
             entry = ingredients3(name=ingredient)
             entry.save()
 
-        with open('recipes3.csv', mode='r') as csv_file:
-            csv_reader = csv.DictReader(csv_file)
-            for row in csv_reader:
-                recipe_name = row['name']
-                time = row['time']
-                if time == '':
-                    time = 0
-                url = row['url'].replace('\'','')
-                print(url)
-                image = row['image']
-                calories = row['calories']
-                if calories == '':
-                    calories = 0
-                recipe = recipes3.objects.create(name=recipe_name,time=time,url=url,image=image,calories=calories)
-                recipe.save()
+        url = 'https://raw.githubusercontent.com/oliviaflexx/recipe/main/django_recipe/mysite/main/recipes3.csv'
+        df = pd.read_csv(url, error_bad_lines=False)
+        for index, row in df.iterrows():
+            recipe_name = row['name']
+            time = row['time']
+            if time == '':
+                time = 0
+            url = row['url'].replace('\'','')
+            print(url)
+            image = row['image']
+            calories = row['calories']
+            if calories == '':
+                calories = 0
+            recipe = recipes3.objects.create(name=recipe_name,time=time,url=url,image=image,calories=calories)
+            recipe.save()
 
-                with open('recipe_ingredients.csv', mode='r') as ing_csv_file:
-                    ing_csv_reader = csv.DictReader(ing_csv_file)
-                    for ing_row in ing_csv_reader:
-                        ing_recipe_name = ing_row['name']
-                        ingredient = ing_row['ingredient']
-                        amount = ing_row['amount']
-                        if not amount:
-                            amount = None
-                        unit = ing_row['unit']
-                        if not unit:
-                            unit = None
-                        if ing_recipe_name == recipe_name:
-                            try:
-                                ingredient_query = ingredients3.objects.get(name=ingredient)
-                                recipe.ingredients.add(ingredient_query)
-                                recipe_ingredients = recipe_ingredients3.objects.create(recipe=recipe, ingredient=ingredient_query, amount=amount, unit=unit)
-                                recipe_ingredients.save()
-                            except ingredients3.DoesNotExist:
-                                print(ingredient)
+            url = 'https://raw.githubusercontent.com/oliviaflexx/recipe/main/django_recipe/mysite/main/recipe_ingredients.csv'
+            df2 = pd.read_csv(url, error_bad_lines=False)
+            for index, ing_row in df2.iterrows():
+                ing_recipe_name = ing_row['name']
+                ingredient = ing_row['ingredient']
+                amount = ing_row['amount']
+                if not amount:
+                    amount = None
+                unit = ing_row['unit']
+                if not unit:
+                    unit = None
+                if ing_recipe_name == recipe_name:
+                    try:
+                        ingredient_query = ingredients3.objects.get(name=ingredient)
+                        recipe.ingredients.add(ingredient_query)
+                        recipe_ingredients = recipe_ingredients3.objects.create(recipe=recipe, ingredient=ingredient_query, amount=amount, unit=unit)
+                        recipe_ingredients.save()
+                    except ingredients3.DoesNotExist:
+                        print(ingredient)
 
-                with open('genres.csv', mode='r') as genre_csv_file:
-                    genre_csv_reader = csv.DictReader(genre_csv_file)
-                    for genre_row in genre_csv_reader:
-                        genre_recipe_name = genre_row['recipe_name']
-                        genre = genre_row['genre']
-                        if genre_recipe_name == recipe_name:
-                            try:
-                                genre_query = genres3.objects.get(name=genre)
-                                recipe.genre.add(genre_query)
-                            except genres3.DoesNotExist:
-                                print(genre_recipe_name)
+            url = 'https://raw.githubusercontent.com/oliviaflexx/recipe/main/django_recipe/mysite/main/genres.csv'
+            df3 = pd.read_csv(url, error_bad_lines=False)
+            for index, genre_row in df3.iterrows():
+                genre_recipe_name = genre_row['recipe_name']
+                genre = genre_row['genre']
+                if genre_recipe_name == recipe_name:
+                    try:
+                        genre_query = genres3.objects.get(name=genre)
+                        recipe.genre.add(genre_query)
+                    except genres3.DoesNotExist:
+                        print(genre_recipe_name)
 
-        with open('or_ingredients.csv', mode='r') as or_csv_file:
-            or_csv_reader = csv.DictReader(or_csv_file)
-            for or_row in or_csv_reader:
-                or_name = or_row['or_name']
-                or_name = ingredients3.objects.get(name=or_name)
-                in_name = or_row['in_name']
-                in_name = ingredients3.objects.get(name=in_name)
-                entry = or_ingredients.objects.create(or_name=or_name, in_name=in_name)
-                entry.save()
+        url = 'https://raw.githubusercontent.com/oliviaflexx/recipe/main/django_recipe/mysite/main/or_ingredients.csv'
+        df4 = pd.read_csv(url, error_bad_lines=False)
+        for index, or_row in df4.iterrows():
+            or_name = or_row['or_name']
+            or_name = ingredients3.objects.get(name=or_name)
+            in_name = or_row['in_name']
+            in_name = ingredients3.objects.get(name=in_name)
+            entry = or_ingredients.objects.create(or_name=or_name, in_name=in_name)
+            entry.save()
                     
         return HttpResponse("Data added")
     else:
