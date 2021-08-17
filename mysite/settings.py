b6d95f4895de157708680bcc5b988cb45c45d4c2
Olivia <oliviaflexx@gmail.com>
2021-08-17 16:12:44 -0400

    collectstatic 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -133,7 +133,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, '_staticfiles')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
