34a423f99bc7fd9d3a1ca8d70b6371876221e766
Olivia <oliviaflexx@gmail.com>
2021-08-17 16:25:53 -0400

    debug false

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 DEBUG_TOOLBAR_CONFIG = {
@@ -142,7 +142,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, "static"),
    ]
-   
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
